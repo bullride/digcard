1c75029fea1a025ad836dbe862dee074f3a4ea3e
Bull <19853236+bullride@users.noreply.github.com>
2021-05-16 04:02:09 +0200

    Update index.vue

--- pages/index.vue
+++ pages/index.vue
@@ -150,7 +150,9 @@
               @click="addAction('primaryActions', index)"
               class="p-3 flex-shrink-0 rounded-full hover:scale-125 focus:scale-125 transform transition-transform duration-200 focus:outline-none"
               :style="{
-                backgroundColor: `${colors.buttonBg.color}`,
+                button.p-3.flex-shrink-0.rounded-full {
+                  border-radius: 25% !important;
+                }backgroundColor: `${colors.buttonBg.color}`,
               }"
               :title="
                 action.name.substr(0, 1).toUpperCase() + action.name.slice(1)
